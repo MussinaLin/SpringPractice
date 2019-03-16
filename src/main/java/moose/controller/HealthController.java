ae365af3b9b1599d2688d4f9b95d5db18320dfce
Mussina Lin <mussina.lin@nogle.com>
2019-03-16 16:13:47 +0800

    actuator practice

--- src/main/java/moose/controller/HealthController.java
+++ src/main/java/moose/controller/HealthController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class WelcomeController implements HealthIndicator {
+public class HealthController implements HealthIndicator {
 
     @Autowired
     private WelcomeService service;
