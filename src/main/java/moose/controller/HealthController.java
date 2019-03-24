50196b12a1862620728969eb8245b21b214100db
Mussina Lin <mussina.lin@nogle.com>
2019-03-24 16:23:54 +0800

    Swagger practice

--- src/main/java/moose/controller/HealthController.java
+++ src/main/java/moose/controller/HealthController.java
@@ -1,19 +1,22 @@
 package moose.controller;
 
+import io.swagger.annotations.ApiOperation;
 import moose.service.WelcomeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("health")
+@RequestMapping("heal")
 public class HealthController implements HealthIndicator {
 
     private int health = 0;
 
-    @RequestMapping("/changeHealth")
+    @ApiOperation(value = "changeHealth", notes = "changeHealth Notes.")
+    @RequestMapping(value = "/changeHealth", method = RequestMethod.POST)
     public String changeHealth(){
         health = health == 0? 1:0;
         return String.valueOf(health);
